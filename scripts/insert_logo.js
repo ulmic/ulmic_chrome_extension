699354b8ffd5256602395b987c3a18ef5505219e
pavel <kalashnikovisme@gmail.com>
2014-06-17 05:35:29 +0400

    fix size of logo

--- scripts/insert_logo.js
+++ scripts/insert_logo.js
@@ -2,6 +2,8 @@ var i;
 console.log("start searching");
 for (i = 0; i < document.links.length; i++) {
   if (document.links[i].innerHTML.toLowerCase().indexOf("#миц") >= 0) {
-    document.links[i].innerHTML += "<img src=\"" + chrome.extension.getURL('images/mini_logo_mic.png') + "\" width=\"15px\" height=\"10px\" />"
+    var style = window.getComputedStyle(document.links[i], null).getPropertyValue('font-size');
+    var fontSize = parseFloat(style);
+    document.links[i].innerHTML += " <img src=\"" + chrome.extension.getURL('images/mini_logo_mic.png') + "\" width=\"auto\" height=\"" + (fontSize + 3) + "px" + "\" />"
   }
 }
